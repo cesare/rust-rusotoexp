7734d02966923868287e3855bace7f2a9a7dbad6
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-08 20:56:14 +0900

    Separate credentials-finder function

--- src/bin/show_credential.rs
+++ src/bin/show_credential.rs
@@ -1,10 +1,16 @@
-use rusoto_credential::{ChainProvider, ProvideAwsCredentials};
+use anyhow::Result;
+use rusoto_credential::{AwsCredentials, ChainProvider, ProvideAwsCredentials};
 use tokio;
+
+async fn find_credentials() -> Result<AwsCredentials> {
+    let provider = ChainProvider::new();
+    let credentials = provider.credentials().await?;
+    Ok(credentials)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let provider = ChainProvider::new();
-    let credentials = provider.credentials().await?;
+    let credentials = find_credentials().await?;
     println!("Found AWS credentials: {:?}", credentials);
     Ok(())
 }
