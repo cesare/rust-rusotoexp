3428d381847325e23487dfd0c8102092a6e988ca
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-11 21:22:25 +0900

    Add s3_list_buckets

---
+++ src/bin/s3_list_buckets.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use rusoto_core::Region;
+use rusoto_s3::{S3, S3Client};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let region = Region::default();
+    let client = S3Client::new(region);
+    let results = client.list_buckets().await?;
+    print!("{:?}", results);
+    Ok(())
+}
