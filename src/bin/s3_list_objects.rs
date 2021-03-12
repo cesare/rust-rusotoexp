5c30ab8447f9a52b1907367e375be20d765514fc
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-12 23:25:56 +0900

    Add s3_list_objects

---
+++ src/bin/s3_list_objects.rs
@@ -0,0 +1,53 @@
+use anyhow::Result;
+use rusoto_core::Region;
+use rusoto_s3::{ListObjectsV2Request, Object, S3Client, S3};
+use structopt::StructOpt;
+use tokio;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "s3_list_objects")]
+struct Options {
+    #[structopt(short, long)]
+    bucket: String,
+}
+
+impl Options {
+    fn create_initial_request(&self) -> ListObjectsV2Request {
+        ListObjectsV2Request {
+            bucket: self.bucket.to_owned(),
+            continuation_token: None,
+            delimiter: None,
+            encoding_type: None,
+            expected_bucket_owner: None,
+            fetch_owner: Some(true),
+            max_keys: None,
+            prefix: None,
+            request_payer: None,
+            start_after: None,
+        }
+    }
+}
+
+fn show_object(object: &Object) {
+    if let Some(key) = &object.key {
+        println!("{}", key);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let options = Options::from_args();
+
+    let region = Region::default();
+    let client = S3Client::new(region);
+    let request = options.create_initial_request();
+    let results = client.list_objects_v2(request).await?;
+
+    if let Some(contents) = results.contents {
+        for object in contents.iter() {
+            show_object(&object);
+        }
+    }
+
+    Ok(())
+}
