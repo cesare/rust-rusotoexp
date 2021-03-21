2705273c94112736e5778ae5d980470cd189ad5e
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-21 20:56:04 +0900

    Add sqs_send_message

---
+++ src/bin/sqs_send_message.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+use rusoto_core::Region;
+use rusoto_sqs::{SendMessageRequest, Sqs, SqsClient};
+use structopt::StructOpt;
+use tokio;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "sqs_receive_message")]
+struct Options {
+    #[structopt(short, long)]
+    queue_url: String,
+
+    #[structopt(short, long)]
+    message: String,
+}
+
+impl Options {
+    fn create_request(&self) -> SendMessageRequest {
+        SendMessageRequest {
+            delay_seconds: None,
+            message_attributes: None,
+            message_body: self.message.to_owned(),
+            message_deduplication_id: None,
+            message_group_id: None,
+            message_system_attributes: None,
+            queue_url: self.queue_url.to_owned(),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let options = Options::from_args();
+
+    let region = Region::default();
+    let client = SqsClient::new(region);
+    let request = options.create_request();
+    let results = client.send_message(request).await?;
+    println!("{:?}", results);
+    Ok(())
+}
